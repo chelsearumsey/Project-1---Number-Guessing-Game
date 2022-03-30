6227ea68c26d53e5ebe46663e9a96d7a2c040297
Chelsea Griffo <chelseagriffo@gmail.com>
2022-03-29 22:04:59 -0500

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -11,23 +11,46 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 
 import random
 
+print("Hello brave soul! I see you are on a quest to guess a random number!")
+
+random_num == random.randint(1,50)
+guess = int(input("I invite thee to take a guess.  "))
+guess_count = 1
 
 def start_game():
-    """Psuedo-code Hints
+	while guess != random_num:
+		if guess > random_num:
+			print("'Tis lower!")
+		elif guess < random_num:
+			print("'Tis higher!")
+		elif guess == random_num:
+			print(f"Huzzah! Thou hast guessed the random number in just {guess_count} attempts!")
+			break
+		guess_count += 1
+
+start_game()
+
+print("Your quest for the random number has ended in triumph! Go forth this day with your head held high!")
+
+
+
+
+
+    #"""Psuedo-code Hints
     
-    When the program starts, we want to:
+    #When the program starts, we want to:
     ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
+    #1. Display an intro/welcome message to the player.
+    #2. Store a random number as the answer/solution.
+    #3. Continuously prompt the player for a guess.
+      #a. If the guess greater than the solution, display to the player "It's lower".
+      #b. If the guess is less than the solution, display to the player "It's higher".
     
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
+    #4. Once the guess is correct, stop looping, inform the user they "Got it"
+        # and show how many attempts it took them to get the correct number.
+    #5. Let the player know the game is ending, or something that indicates the game is over.
     
-    ( You can add more features/enhancements if you'd like to. )
+    #( You can add more features/enhancements if you'd like to. )
     """
     # write your code inside this function.
 
