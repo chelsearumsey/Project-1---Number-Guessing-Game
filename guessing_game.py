a6fd4f63e0cdcfe98b5cedc14cd2835bde50254e
Chelsea Griffo <chelseagriffo@gmail.com>
2022-12-04 17:16:22 -0600

    Added high_score global variable

--- guessing_game.py
+++ guessing_game.py
@@ -12,8 +12,10 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 import random
 
 print("Hello brave soul! I see you are on a quest to guess the random number!")
+high_score = 100
 
 def start_game():
+	global high_score # w3schools.com/python/python_variables_global.asp
 	num = random.randint(1, 50) 
 	guess_count = 1
 	guess = input("I invite thee to guess a number between 1 and 50.  ")
@@ -43,48 +45,21 @@ def start_game():
 	while True:
 		try:
 			guess_again = input("Wouldst thou like to guess another number? (Enter 'yes' or 'no')  ").lower()
-		
-		except ValueError:
-			print("Please answer 'yes' or 'no'.  ") 
 
-		else:
 			if guess_again == "yes": 
+				if guess_count <= high_score:
+					high_score = guess_count
+					print(f"Thy current best score is {high_score}!")
 				start_game() 
 				break
 			elif guess_again =="no": 
 				print("Your quest for the random number has ended in triumph! Go forth this day with your head held high!") 
 				break
-				
-	
+			else:
+				raise IndexError
+		
+		except IndexError:
+			print("Please answer 'yes' or 'no'.  ") 
 
 
 start_game()
-
-
-
-
-
-
-
-
-	#"""Psuedo-code Hints
-	
-	#When the program starts, we want to:
-   # ------------------------------------
-	#1. Display an intro/welcome message to the player.
-	#2. Store a random number as the answer/solution.
-	#3. Continuously prompt the player for a guess.
-	  #a. If the guess greater than the solution, display to the player "It's lower".
-	  #b. If the guess is less than the solution, display to the player "It's higher".
-	
-	#4. Once the guess is correct, stop looping, inform the user they "Got it"
-		# and show how many attempts it took them to get the correct number.
-	#5. Let the player know the game is ending, or something that indicates the game is over.
-	
-	#( You can add more features/enhancements if you'd like to. )
-   # """
-	# write your code inside this function.
-
-
-
-# Kick off the program by calling the start_game function.
